8a37bc27a92a0beaa054f17358acf128a6f4e9ff
Brandon Hartshorn <brandonhartshorn@gmail.com>
2017-09-28 11:10:50 -0600

    Now uses a loop and an array to demonstrate that printing can get mixed up

--- cpp11_practice.cpp
+++ cpp11_practice.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <thread>
+#include <array>
 #include <string>
 
 using namespace std;
 
 static const int num_threads = 10;
-static const string test_string[] = {"test", "this", "string", "array", "please"};
-
+static const array <string, 5> test_string = {"test", "this", "string", "array", "please"}; 
 void test_print() {
-	std::cout << "This is a fairly long stream so as to induce printing errors" << std::endl;
+	for (int i = 0; i < test_string.size(); ++i) {
+		cout << test_string[i] << endl;
+	}
+	cout << endl;
 }
 
 int main() {
-	std::thread t[num_threads];
+	thread t[num_threads];
 
 	for (int i = 0; i < num_threads; ++i) {
-		t[i] = std::thread(test_print);
+		t[i] = thread(test_print);
 	}
 
-	std::cout << "Main thread exiting" << std::endl;
+	cout << "Main thread exiting" << endl;
 
 	for (int i = 0; i < num_threads; ++i) {
 		t[i].join();
