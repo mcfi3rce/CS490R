be80eb514969015d4c58c82627f3f152ab80e281
Brandon Hartshorn <brandonhartshorn@gmail.com>
2017-10-05 15:59:41 -0600

    fridge is in business

--- pthread_refridgerator.cpp
+++ pthread_refridgerator.cpp
@@ -1,119 +1,79 @@
 #include <pthread.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <iostream>
 #include <string>
-#include <ctime>
 #include <deque>
-#include <stack>
 
 using namespace std;
 
-string goodFoods[] = {"Hummus", "Avocado Toast", "Peanut Butter", "Tofu Curry", "Guacamole", "Salsa", "Hash Browns"};
+string goodFoods[] = {"Hummus", "Avocado Toast", "Peanut Butter", "Tofu Curry", "Guacamole", "Salsa", "Soda"};
 
-class Food {
-	public:
-		Food() {
-			validate("Raw Tofu", 3);
-		}
-		Food(string type) {
-			validate(type, 3);
-		}
-		Food(string type, int timeToExpiration) {
-			validate(type, timeToExpiration);
-		}
-		string getType() const;
-		bool isExpired() const {
-			return time(0) > expirationTime;
-		}
-	private:
-		void validate(string type, int timeToExpiration) {
-			type = type;
-			expirationTime = time(0) + timeToExpiration;
-		}
-		string type;
-		time_t expirationTime;
-};
+deque<string> fridge;
 
-class Fridge {
-	public:
-		Fridge() { }
-		int addFood(Food food) {
-			add(food);
+int itemsInFridge = 0;
+int fridgeSize = 4;
+int numItems = 10;
+
+pthread_cond_t foodAvailable, fridgeEmpty;
+pthread_mutex_t openFridge;
+
+void putInFridge(string food) {
+	if (fridge.size() >= fridgeSize) {
+		cout << "\033[33mFridge is full! " << food << " was left on the counter to mold!\033[0m\n";
+		return;
+	}
+	cout << "\033[32mPut " << food << " in the fridge for guests\033[0m\n";
+	fridge.push_back(food);
+}
+
+void takeFromFridge(int greed, long threadid) {
+	for (int i = 0; i < greed; ++i) {
+		if (fridge.empty()) {
+			cout << "\033[31mThread " << threadid << ": Fridge is empty, so hungry....\033[0m\n";
+		} else {
+			string toEat = fridge.front();
+			fridge.pop_front();
+			cout << "\033[34mThread " << threadid << ": That " << toEat << " was delicious!\033[0m\n";
 		}
-		Food takeFood(int num) {
-			return Food();
-		}
-		int foodRemaining() {
-			return 0;
-		}
-	private:
-		void add(Food item) {}
-};
-/*
-void *test_function(void *param)
-{
-	int *param_value = (int *)param;
-	while(++(*param_value) < 100)
-	{
-		sleep(1);
+	}
+	return;
+}
+
+void *host(void* args) {
+	for (int i = 0; i < numItems * 2; ++i) {
+		int foodId = rand() % sizeof(goodFoods)/sizeof(*goodFoods);
+		pthread_mutex_lock(&openFridge);
+		putInFridge(goodFoods[foodId]);
+		pthread_mutex_unlock(&openFridge);
+		sleep(rand() % 2);
 	}
 
-	printf("did the thing\n");
-
-	return NULL;
 }
-*/
+
+void *guest(void* args) {
+	long id = (long) args;
+	for (int i = 0; i < numItems; ++i) {
+		pthread_mutex_lock(&openFridge);
+		takeFromFridge(1, id);
+		pthread_mutex_unlock(&openFridge);
+		sleep(rand() % 4);
+	}
+
+}
 
 int main()
 {
-	//int x = 0, y = 0;
+	pthread_mutex_init(&openFridge, NULL);
+	pthread_t hostThread, guestThread1, guestThread2;
 
-	/* show the initial values of x and y */
-	//printf("x: %d, y: %d\n", x, y);
+	pthread_create(&hostThread, NULL, host, (void *)NULL);
+	pthread_create(&guestThread1, NULL, guest, (void *)1);
+	pthread_create(&guestThread2, NULL, guest, (void *)2);
+	pthread_join(hostThread,NULL);
+	pthread_join(guestThread1,NULL);
+	pthread_join(guestThread2,NULL);
 
-	/* this variable is our reference to the second thread */
-	//pthread_t inc_x_thread;
-
-	/* create a second thread which executes inc_x(&x) */
-	/*
-	if(pthread_create(&inc_x_thread, NULL, test_function, &x)) {
-
-		fprintf(stderr, "Error creating thread\n");
-		return 1;
-
-	}
-	*/
-	/* increment y to 100 in the first thread */
-	//while(++y < 100);
-
-	//printf("y increment finished\n");
-
-	/* wait for the second thread to finish */
-	/*
-	if(pthread_join(inc_x_thread, NULL)) {
-
-		fprintf(stderr, "Error joining thread\n");
-		return 2;
-
-	}
-	*/
-
-	/* show the results - x is now 100 thanks to the second thread */
-	//printf("x: %d, y: %d\n", x, y);
-
-	cout << goodFoods << endl;
-	cout << sizeof(goodFoods) / sizeof(*goodFoods) << endl;
-	time_t test = time(NULL);
-
-	cout << test << endl;
-
-	Food yum = Food("Hummus", 1);
-
-	cout << yum.isExpired() << endl;
-
-	sleep(2);
-
-	cout << yum.isExpired() << endl;
 
 	return 0;
 }
